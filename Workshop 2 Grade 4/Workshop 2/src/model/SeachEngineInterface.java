4d57d06e2d8c2a4e1376879a4b3591fe15ead33c
vikrantmainali <vikrantmainali@hotmail.com>
2018-03-22 17:19:00 +0100

    deleted old files

--- Workshop 2 Grade 4/Workshop 2/src/model/SeachEngineInterface.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import java.util.ArrayList;
-
-public interface SeachEngineInterface{
-
-	public ArrayList<Member> searchPersonalNumber(RegistryContainer rc,String personalNumber); //we are searching base on the personal number
-	public ArrayList<Member> searchEverything(RegistryContainer rc,String name,String id,String pn);
-}
